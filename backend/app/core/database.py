c25635455be852e5e99aed2bef0c64c9d832ffea
Abdul Rahiman <rahimanks.abdul>
2026-07-06 00:36:39 +0530

    fix: update Supabase database connection string format

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -5,7 +5,7 @@ import os
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./mindful_spend.db"
 # For PostgreSQL: SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Aa828118$sainu@db.wfjylbsulavpqxyscuvh:Aa828118$sainu@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.wfjylbsulavpqxyscuvh:Aa828118$sainu@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
